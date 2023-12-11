9fdca708cd2569d59e507bead609092a17622804
nurtaazim <nurtaazimmukanov24@gmail.com>
2023-12-11 17:57:23 +0600

    test commit

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("test");
         Scanner scanner = new Scanner(System.in);
         Scanner scannerForNum = new Scanner(System.in);
         DataBase dataBase = new DataBase();
